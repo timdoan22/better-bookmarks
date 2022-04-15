5571f9dc75ffd29d7c89284d380e1504191c8a70
timdoan22 <timdoan01@gmail.com>
2022-04-16 00:00:00 +0900

    init packages and server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const logger = require("morgan");
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
