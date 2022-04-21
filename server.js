6d6999ee6d9bcaaed8d8adc660a27d7ab8f1ea70
timdoan22 <timdoan01@gmail.com>
2022-04-22 00:00:00 +0900

    main routes

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
+const flash = require("express-flash");
 const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const bookmarkRoutes = require("./routes/bookmarks");
+const commentRoutes = require("./routes/comments");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -47,6 +54,12 @@ app.use(passport.session());
 //Use flash messages for errors, info, ect...
 app.use(flash());
 
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+app.use("/bookmark", bookmarkRoutes);
+app.use("/comment", commentRoutes);
+
+//Server Running
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
 });
