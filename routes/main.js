5a7ed84c3dd3fde49f97fd86ef34d6bac9ad992f
timdoan22 <timdoan01@gmail.com>
2022-06-15 00:00:00 +0900

    signup routes in main

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,7 @@ const homeController = require("../controllers/home");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
 module.exports = router;
