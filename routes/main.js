3b6d87d988679479f51a131571fb38d48dfea7d6
timdoan22 <timdoan01@gmail.com>
2022-06-17 00:00:00 +0900

    login routes in main

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,8 @@ const homeController = require("../controllers/home");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
 
