628b1f6354a555e40ca444706fa18bd0d43d0526
timdoan22 <timdoan01@gmail.com>
2022-06-22 00:00:00 +0900

    postLogin auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,3 +14,19 @@ exports.getLogin = (req, res) => {
   });
 };
 
+exports.postLogin = (req, res, next) => {
+  // validation to check for valid email and password
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (validator.isEmpty(req.body.password))
+    validationErrors.push({ msg: "Password cannot be blank." });
+  // flash the error messages for incorrect email and password
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    return res.redirect("/login");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+}
