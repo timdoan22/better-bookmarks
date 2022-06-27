a13934d2d7f08622b71bb97a19fa40b093bfb2fe
timdoan22 <timdoan01@gmail.com>
2022-06-28 00:00:00 +0900

    postSignup auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,6 +62,28 @@ exports.getSignup = (req, res) => {
   });
   };
 
+  // sign up logic
+exports.postSignup = (req, res, next) => {
+  // validators for email and password data
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (!validator.isLength(req.body.password, { min: 8 }))
+    validationErrors.push({
+      msg: "Password must be at least 8 characters long",
+    });
+  if (req.body.password !== req.body.confirmPassword)
+    validationErrors.push({ msg: "Passwords do not match" });
+  // flash sign up errors 
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    // redirect user back to the /signup route
+    return res.redirect("../signup");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
 // schema model for the user document to save into the database
 const user = new User({
   userName: req.body.userName,
